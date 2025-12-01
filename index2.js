1d48c028cde6ed756ee29c51c38ecd1ebde2d233
EduardoB20 <eduardobrandao1020@gmail.com>
2025-12-01 20:12:43 -0300

    Add files via upload
    Primeiro Add

---
+++ index2.js
@@ -0,0 +1,64 @@
+/*const readline = require('readline-sync');  
+let x = 0
+console.log("Bem Vindo Ao Programa!")
+do{
+    let x = parseInt(readline.question("Digite 1 para encerrar; 2 para continuar: \n"))
+    if (x == 1){
+        break;
+    } else{
+        console.log("X diferente de 1!")
+    }
+}while(x !=1)   
+console.log("Programa encerrado")
+*/
+/*
+const readline = require('readline-sync')
+
+console.log("Bem Vindo Ao Programa!")
+let fruta = parseInt(readline.question("Digite 1 para comprar Laranja; Digite 2 para comprar Pera; Digite 3 para nao comprar nada\n"))
+
+switch(fruta){
+    case 1:
+        console.log("Voce comprou laranjas")
+        break
+    case 2:
+        console.log("Voce comprou peiras!")
+        break
+    default:
+        console.log("Hmm, esse item nao esta listado")
+}
+console.log("Isso ai")
+*/
+/*
+const readline = require('readline-sync');
+console.log("Bem Vindo Ao Programa!")
+let Numero = parseInt(readline.question("Digite o número"))
+switch(Numero){
+    case 1:
+        console.log ("1 é Ímpar")
+        break;
+    case 2:
+        console.log("2 é par")
+        // COMENTARIO: Deveria existir um break aqui!
+    case 3:
+        console.log("3 é Ímpar")
+        // já que nao existe ele continua até o proximo break(esse aqui)
+        break;
+    default:
+        console.log("Não sei que número é esse!")
+        break;
+}
+console.log("Programa encerrado")   
+ */
+ 
+
+let x = 1
+console.log("Bem Vindo ao programa")
+while(x<5){
+ㅤconsole.log(`Olha, o número atual é: ${x}`)
+ㅤx+=1
+}
+
+
+
+
